4e3909fbe3819270d8cc35b2f624d70137731107
Dan <Danhamilton1111@gmail.com>
2013-06-17 13:46:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letsfleelaunch::Application.config.secret_key_base = 'f7f2e69dcdb906faeb2f61e8a581d8962116d4c1516b498671d313cb4421accfbba322ffe2ebaee094f5ca24377ba581870013794ab32d695e0cc9d21edc154b'
