a474bf4141f6a7d42157a7a9724823ad4651502c
Dan <Danhamilton1111@gmail.com>
2013-06-18 01:40:21 -0400

    added fb auth, but haven't changed views. Also not accessing open graph

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130618034745) do
+ActiveRecord::Schema.define(version: 20130618052006) do
 
   create_table "interests", force: true do |t|
     t.datetime "created_at"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20130618034745) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "oauth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
