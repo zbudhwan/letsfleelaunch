8b27eabeadf4429b4858e0494b93e5b33fe9c216
Dan <Danhamilton1111@gmail.com>
2013-06-18 13:33:51 -0400

    changed dev.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,8 @@ Letsfleelaunch::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  ENV['FACEBOOK_APP_ID'] =  '146253768900648'
-  ENV['FACEBOOK_APP_SECRET'] = '8651907ca26be9802bbce786563e2891'
+  ENV['FACEBOOK_APP_ID'] = '496755510395510'
+  ENV['FACEBOOK_APP_SECRET'] = '96e18485ac4c883d96488ce6b578de6c'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
